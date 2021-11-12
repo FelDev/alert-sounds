31e70f126ef5798e6e6efeb25d3c06026d1f2cbf
Felix Paradis <allo@felixparadis.com>
2021-11-12 12:52:59 -0600

    reorganize files, cleanup

--- randomize.py
+++ randomize.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
 import subprocess, random, glob
-print('ehlala')
 
-sounds = glob.glob('/Users/felix/Library/Sounds/*.aiff')
+sounds = glob.glob('/Users/felix/Library/Sounds/audio/*.aiff')
 sound = random.choice(sounds)
-print('randomly selected sound: ', sound)
+# print('randomly selected sound: ', sound)
 command = 'defaults write .GlobalPreferences com.apple.sound.beep.sound {}'.format(sound)
-# sounds=['tabarnak1', 'tabarnak2', 'tabarnak3']
-# sound=random.choice(sounds)
 
-# command = 'defaults write .GlobalPreferences com.apple.sound.beep.sound /Users/felix/Library/Sounds/{}.aiff'.format(sound)
-# subprocess.call(' defaults write .GlobalPreferences com.apple.sound.beep.sound /System/Library/Sounds/Submarine.aiff ', shell=True)
 subprocess.call(command, shell=True)
